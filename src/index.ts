ed2e415ebb8ce5693f94d7b38ac5df9135063239
Ianis Triandafilov <janis.sci@gmail.com>
2022-10-28 19:32:42 +0200

    check files

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ export default dig;
  * A dig function that takes any object with a nested structure and a path,
  * and returns the value under that path, or undefined when not found.
  *
- *
  * @param {any}     source - A nested objects.
  * @param {string}  path - A path string, for example `my[1].test.field`
  * @param {boolean} [shouldThrow=false] - Optionally throw an exception when nothing found
